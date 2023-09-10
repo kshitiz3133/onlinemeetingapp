e053ea11543a05b774c33447b67e27ae37e3d606
kshitiz <kshitizagarwal3133@gmail.com>
2023-09-11 02:44:47 +0530

    Settings page added and buttons working

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:onlinemeetingapp/Home/homepage.dart';
 import 'FloatingBar/fbarcall.dart';
-class App extends StatelessWidget {
+import 'Settings/Settings.dart';
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+  @override
+  State<App> createState() => _AppState();
+}
 
+class _AppState extends State<App> {
+  final lul = PageController(initialPage: 0);
+  @override
+  void  dispose(){
+    lul.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          // call homepage at the start
-          Homepage(),
+          //Homepage and settings page
+          PageView(
+            controller: lul,
+            children: [
+              Homepage(),
+              Settings(),
+            ],
+              onPageChanged: (index){}
+          ),
           // floatingbar at the top of stack(at the top of all pages)
-          Fbarcall()
+          Fbarcall(),
+          Positioned(
+            bottom: 40,
+            left: 80,
+            child: IconButton(onPressed: (){
+              lul.animateToPage(0, duration: Duration(milliseconds: 200), curve: Curves.linear);
+            }, icon: Icon(Icons.home)),
+          ),
+          Positioned(
+            bottom: 40,
+            right: 80,
+            child: IconButton(onPressed: (){
+              lul.animateToPage(1, duration: Duration(milliseconds: 200), curve: Curves.linear);
+            }, icon: Icon(Icons.settings)),
+          )
         ],
       ),
     );
