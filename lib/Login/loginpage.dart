b6c2ac505399aae6bc0f4b116c61c82ef0eeaee4
kshitiz <kshitizagarwal3133@gmail.com>
2023-09-11 02:53:20 +0530

    Settings page added and buttons working

--- lib/Login/loginpage.dart
+++ lib/Login/loginpage.dart
@@ -6,6 +6,7 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
